1a20258bc1262fc158b2f8b097b15378f2898aa2
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-04-02 16:01:32 +0700

    UPDATE: Single product page - Sidebar block - Changed class name - Added full width option for button

--- gpw-templates/woocommerce/single-product/product-details/sidebar-block.php
+++ gpw-templates/woocommerce/single-product/product-details/sidebar-block.php
@@ -7,10 +7,13 @@ global $product;
 $price = $product->get_price();
 $moveToSectionID = isset($args['move_to']) ? $args['move_to'] : '';
 ?>
-<div class="product-sidebar__price">
+<div class="product-sidebar product-sidebar--price">
   <?= $product->get_price_html() ?>
   
   <?php if( !empty( $moveToSectionID )) {
-    get_template_part( 'gpw-templates/global/gpw-button', null, [ 'style' => 'primary', 'url' => "#{$moveToSectionID}", 'label' => __('Chọn các gói dịch vụ', 'gpw')]);
+    get_template_part( 'gpw-templates/global/gpw-button', null, [ 'style' => 'primary', 'url' => "#{$moveToSectionID}", 'label' => __('Chọn các gói dịch vụ', 'gpw'), 'width' => 'full']);
   } ?>
 </div>
+<?php 
+// ! Unset variables
+unset($price, $moveToSectionID);
