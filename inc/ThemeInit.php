b0e36aa715007d838eb6a78262d28b21d3b23c3d
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-04-13 10:35:41 +0700

    UPDATE: Registered overrideWoocommerce controller

--- inc/ThemeInit.php
+++ inc/ThemeInit.php
@@ -18,6 +18,7 @@ class ThemeInit {
       controller\CompanyInfo::class,
       woocommerce\BookingController::class,
       woocommerce\CartAndCheckoutController::class,
+      woocommerce\OverrideFlatsomeAndWoocommerceHooks::class,
     ];
   }
 
